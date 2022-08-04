a3951523578e427c83e8e819416cf30d33ef449b
TheTrueZK <thetruezk@gmail.com>
2022-08-04 11:09:12 -0500

    added first question

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js')
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'projectTitle'
+        message: "What's the name of your project?"
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter a name')
+                return false;
+            }
+        }
+    },
+    
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
